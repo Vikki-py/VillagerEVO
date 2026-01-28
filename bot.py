5b26b6069501558c5236b6d70f9ea43e30578d9d
Vikki-py <lobovnikolay34@gmail.com>
2026-01-28 20:27:38 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ import os
 
 async def main():
     bot = Bot(
-        token=os.getenv('BOT_TOKEN'),
+        token="7845070452:AAHuaQo_3x_n3e878kLG1B2QRRSCmT26C9k",
         default=DefaultBotProperties(parse_mode=ParseMode.HTML)
     )
     dp = Dispatcher()
